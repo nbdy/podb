2eb1468e6c1c1d238d49a0b49ce358bc066550a2
Pascal Eberlein <pascal@eberlein.io>
2021-01-30 13:43:09 +0100

    updated

--- tests/simple.py
+++ tests/simple.py
@@ -4,7 +4,9 @@ from random import choice, randint, uniform
 from string import printable
 import unittest
 import pickle
-from copy import copy
+
+TBL = "test"
+db = DB("test")
 
 
 def generate_string(n=20, charset=printable):
@@ -26,51 +28,49 @@ class TestObject(DBEntry):
 class DBTestMethods(unittest.TestCase):
     def test_insert(self):
         to = TestObject.random()
-        b = tbl.size()
-        tbl.insert(to)
-        a = tbl.size()
+        b = db.size(TBL)
+        db.insert(TBL, to)
+        a = db.size(TBL)
         self.assertGreater(a, b, "DB entry count increased")
 
     def test_get_kv(self):
         to = TestObject.random()
-        tbl.insert(to)
-        ti = tbl.get_by("name", to.name)
+        db.insert(TBL, to)
+        ti = db.get_by(TBL, {"name": to.name})
         self.assertIsNotNone(ti, "get_kv returned not None")
         self.assertEqual(ti.uuid, to.uuid, "Inserted and retrieved object uuids are the same")
 
     def test_get_by_object(self):
         to = TestObject.random()
         tp = pickle.dumps(to)
-        tbl.insert(to)
-        ti = tbl.get_by("object", tp)
+        db.insert(TBL, to)
+        ti = db.get_by(TBL, {"object": tp})
         self.assertIsNotNone(ti, "get_kv by object is not None")
         tip = pickle.dumps(ti)
         self.assertEqual(tp, tip, "inserted and retrieved pickle representations are equal")
 
     def test_get_uuid(self):
         to = TestObject.random()
-        tbl.insert(to)
-        ti = tbl.get_by_uuid(to.uuid)
+        db.insert(TBL, to)
+        ti = db.get_by_uuid(TBL, to.uuid)
         self.assertIsNotNone(ti, "get_uuid returned not None")
         self.assertEqual(to.uuid, ti.uuid, "Inserted and retrieved object uuids are the same")
 
     def test_update(self):
         to = TestObject.random()
-        tbl.insert(to), "database insert"
+        db.insert(TBL, to), "database insert"
         toageb = int(to.age)
         tosizeb = float(to.size)
         to.age *= 2
         to.size *= 2
         self.assertNotEqual(toageb, to.age, "doubled the age")
         self.assertNotEqual(tosizeb, to.size, "doubled the size")
-        tbl.update(to)
-        ti = tbl.get_by_uuid(to.uuid)
+        db.update(TBL, to)
+        ti = db.get_by_uuid(TBL, to.uuid)
         self.assertIsNotNone(ti, "get_uuid returned not None")
         self.assertGreater(ti.size, tosizeb, "greater than original objects age")
         self.assertGreater(ti.age, toageb, "greater than original objects age")
 
 
 if __name__ == '__main__':
-    db = DB("test")
-    tbl = db.t("testTbl")
     unittest.main()
