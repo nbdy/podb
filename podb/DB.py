3e4c91e0785329b80e16ba25cffe635cdc285799
Pascal Eberlein <pascal@eberlein.io>
2021-02-07 20:57:50 +0100

    moving to shelve

--- podb/DB.py
+++ podb/DB.py
@@ -1,7 +1,5 @@
-import pickle
-from multiprocessing import Lock
 from uuid import uuid4
-import dataset
+import shelve
 from datetime import datetime
 
 
@@ -11,129 +9,86 @@ FMT_TIMESTAMP = "%Y.%m.%d %H:%M:%S"
 class DBEntry(object):
     def __init__(self, **kwargs):
         self.uuid = str(uuid4())
-        self.created = self.get_timestamp()
+        self.created = datetime.now()
         self.last_modified = self.created
         self.__dict__.update(kwargs)
 
-    @staticmethod
-    def get_timestamp(fmt=FMT_TIMESTAMP):
-        return datetime.now().strftime(fmt)
-
-
-class Serializer(object):
-    @staticmethod
-    def serialize(obj: DBEntry):
-        ctx = {
-            "object": pickle.dumps(obj),
-            # "functions": []
-        }
-        for k, v in obj.__dict__.items():
-            for t in [str, int, float, bytes, bool, complex, datetime]:
-                if isinstance(v, t):
-                    ctx[k] = v
-            '''
-            if hasattr(v, '__call__'):
-                ctx.__setitem__("functions", v.__name__)
-                # ctx["functions"].append(v.__name__)
-            elif isinstance(v, dict):
-                ctx[k] = DB.serialize(v)
-            elif isinstance(v, object):
-                ctx[k] = DB.serialize(v.__dict__)
-            '''
-        return ctx
-
-    @classmethod
-    def serialize_many(cls, lst: list[DBEntry]):
-        r = []
-        for i in lst:
-            r.append(cls.serialize(i))
-        return r
-
 
 class DB(object):
-    mtx: Lock = Lock()
-    ser: Serializer = None
-
-    def __init__(self, name: str, serializer: Serializer = Serializer):
-        self.ser = serializer
-        self.db = dataset.connect("sqlite:///{}.db".format(name),
-                                  engine_kwargs={"connect_args": {"check_same_thread": False}})
+    def __init__(self, name: str):
+        self.db = shelve.open(name)
 
     @staticmethod
-    def get_object(r):
-        if r:
-            return pickle.loads(r["object"])
+    def has_keys(d: dict, keys: list[str]) -> bool:
+        for k in keys:
+            if k not in d.keys():
+                return False
+        return True
+
+    @staticmethod
+    def k_v_match(d: dict, c: dict):
+        if not DB.has_keys(d, list(c.keys())):
+            return False
+        for k in c.keys():
+            if d[k] != c[k]:
+                return False
+        return True
+
+    @staticmethod
+    def update_values(o: DBEntry, n: DBEntry):
+        for k, v in n.__dict__.items():
+            o.__dict__[k] = v
+        return o
+
+    def insert(self, o: DBEntry, upsert=False):
+        if o.uuid in self.db.keys() and not upsert:
+            return False
+        self.db[o.uuid] = o
+        self.db.sync()
+        return True
+
+    def insert_many(self, lst: list[DBEntry], upsert=False):
+        for e in lst:
+            if not self.insert(e, upsert):
+                return False
+        return True
+
+    def update(self, o: DBEntry, upsert=False):
+        if o.uuid not in self.db.keys() and not upsert:
+            return False
+        old = self.db[o.uuid]
+        o.last_modified = datetime.now()
+        o = self.update_values(old, o)
+        self.db[o.uuid] = o
+        self.db.sync()
+        return True
+
+    def upsert(self, o: DBEntry):
+        if o.uuid in self.db.keys():
+            o.last_modified = datetime.now()
+            self.db[o.uuid].__dict__.update(o.__dict__)
+        else:
+            self.db[o.uuid] = o
+        self.db.sync()
+
+    def find(self, query: dict):
+        def matches(v: dict):
+            return self.k_v_match(v, query)
+        return {k: v for k, v in self.db.items() if matches(v.__dict__)}
+
+    def find_by_uuid(self, uuid):
+        return self.db[uuid]
+
+    def find_one(self, query: dict):
+        r = self.find(query)
+        if len(r.keys()) > 0:
+            return list(r.values())[0]
         return None
 
-    def get_by(self, tbl: str, key: str, value):
-        r = []
-        for i in self.db[tbl].find(**{key: value}):
-            r.append(self.get_object(i))
-        return r
+    def find_after(self, timestamp: datetime, key="created"):
+        def matches(v: dict):
+            return v[key] >= timestamp
+        return {k: v for k, v in self.db.items() if matches(v.__dict__)}
 
-    def get_one_by(self, tbl: str, key: str, value):
-        return self.get_object(self.db[tbl].find_one(**{key: value}))
-
-    def get_by_uuid(self, tbl: str, uuid: str):
-        return self.get_one_by(tbl, "uuid", uuid)
-
-    def get_after(self, tbl: str, after: datetime, count: int = 10):
-        with self.mtx:
-            t = self.db[tbl]
-            d = t.find(t.table.columns.last_modified >= after.strftime(FMT_TIMESTAMP), _limit=count,
-                       order_by=["last_modified"])
-        r = []
-        for i in d:
-            r.append(self.get_object(i))
-        return r
-
-    def contains(self, tbl: str, key: str, value):
-        with self.mtx:
-            r = self.db[tbl].count(**{key: value})
-        return r > 0
-
-    def serialize(self, o: DBEntry):
-        return self.ser.serialize(o)
-
-    def serialize_many(self, lst: list[DBEntry]):
-        return self.ser.serialize_many(lst)
-
-    def size(self, tbl: str):
-        with self.mtx:
-            r = self.db[tbl].count(**{})
-        return r
-
-    def get_tables(self):
-        return self.db.tables
-
-    def get_columns(self, tbl: str):
-        return self.db[tbl].columns
-
-    def count(self, tbl: str, key: str, value):
-        with self.mtx:
-            r = self.db[tbl].count(**{key: value})
-        return r
-
-    def insert(self, tbl: str, o: DBEntry):
-        with self.mtx:
-            self.db[tbl].insert(self.serialize(o))
-
-    def insert_many(self, tbl: str, lst: list[DBEntry]):
-        with self.mtx:
-            self.db[tbl].insert_many(self.serialize_many(lst))
-
-    def update(self, tbl: str, o: DBEntry):
-        with self.mtx:
-            self.db[tbl].update(self.serialize(o), ["uuid"])
-
-    def upsert(self, tbl: str, o: DBEntry):
-        with self.mtx:
-            self.db[tbl].upsert(self.serialize(o), ["uuid"])
-
-    def upsert_many(self, tbl: str, lst: list[DBEntry]):
-        with self.mtx:
-            self.db[tbl].upsert_many(self.serialize_many(lst), ["uuid"])
-
-    def remove(self, tbl: str, o: DBEntry):
-        with self.mtx:
-            self.db[tbl].delete(**{"uuid": o.uuid})
+    def size(self):
+        return len(self.db.keys())
